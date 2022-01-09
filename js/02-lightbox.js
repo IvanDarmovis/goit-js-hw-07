618b8a97bc20db169d6de0461d7b25ebe0a749bd
IvanDarmovis <ivan.darmovis1@gmail.com>
2022-01-09 16:58:49 +0200

    second task complete

--- js/02-lightbox.js
+++ js/02-lightbox.js
@@ -1,4 +1,29 @@
-import { galleryItems } from './gallery-items.js';
+import { galleryItems } from "./gallery-items.js";
 // Change code below this line
 
 console.log(galleryItems);
+
+const galleryEl = document.querySelector(".gallery");
+
+const markup = galleryItems
+  .map(
+    ({
+      preview,
+      original,
+      description,
+    }) => `<a class="gallery__item" href="${original}">
+  <img class="gallery__image" src="${preview}" alt="${description}" />
+</a>`
+  )
+  .join("\n");
+
+galleryEl.insertAdjacentHTML("beforeend", markup);
+new SimpleLightbox(".gallery a", {
+  captioneType: "text",
+  captionsData: "alt",
+  captionDelay: 250,
+});
+
+document.addEventListener("keydown", (e) => {
+  if (e.code === "Escape") close.SimpleLightbox;
+});
