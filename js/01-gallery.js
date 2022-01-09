432ae85378c9cf3066cd39df3a777292ff2f526e
IvanDarmovis <ivan.darmovis1@gmail.com>
2022-01-09 15:11:00 +0200

    first task complete

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -1,4 +1,43 @@
-import { galleryItems } from './gallery-items.js';
+import { galleryItems } from "./gallery-items.js";
 // Change code below this line
 
 console.log(galleryItems);
+const galleryEl = document.querySelector(".gallery");
+
+const markup = galleryItems
+  .map(
+    ({ preview, original, description }) => `<div class="gallery__item">
+  <a class="gallery__link" href="${original}">
+    <img
+      class="gallery__image"
+      src="${preview}"
+      data-source="${original}"
+      alt="${description}"
+    />
+  </a>
+</div>`
+  )
+  .join("\n");
+
+galleryEl.insertAdjacentHTML("beforeend", markup);
+galleryEl.addEventListener("click", onClickPic);
+
+function onClickPic(e) {
+  e.preventDefault();
+
+  if (!e.target.classList.contains("gallery__image")) return;
+  showBigImg(e);
+}
+
+function showBigImg(e) {
+  const url = e.target.dataset.source;
+  const instance = basicLightbox.create(`<img src="${url}">`);
+  instance.show();
+  closeBigImageByEsc(instance);
+}
+
+function closeBigImageByEsc(inst) {
+  document.addEventListener("keydown", (e) => {
+    if (e.code === "Escape") inst.close();
+  });
+}
